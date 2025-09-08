05cbc91fc65c3aec52f63e714d86345a8ec9e693
DaveMcBlame1 <165719301+DaveMcBlame1@users.noreply.github.com>
2025-09-08 13:51:06 -0500

    fard

--- WWW/index.php
+++ WWW/index.php
@@ -3,12 +3,15 @@ $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
     case '/':
+    case '':
+        require __DIR__ . '/index.html';
+        break;
     case '/pages/policy':
         require __DIR__ . '/pages/policy.html';
         break;
     default:
         http_response_code(404);
-        require __DIR__ . '/404.php';
+        require __DIR__ . 'errors/404.html';
         break;
 }
 ?>
