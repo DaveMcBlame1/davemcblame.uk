f623016e8cbdbdf809b1544063b9a86b46f2d6da
DaveMcBlame1 <165719301+DaveMcBlame1@users.noreply.github.com>
2025-09-08 13:28:22 -0500

    idk

--- WWW/index.php
+++ WWW/index.php
@@ -1,1 +1,17 @@
-<?php phpinfo(); ?>
+<?php
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/':
+    case '':
+        require __DIR__ . '/home.php';
+        break;
+    case '/pages/policy':
+        require __DIR__ . '/pages/policy.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/404.php';
+        break;
+}
+?>
