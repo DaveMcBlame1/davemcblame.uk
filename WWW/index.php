26e9a3aed42a9b8d0144c0be13a4a4245d04a832
DaveMcBlame1 <165719301+DaveMcBlame1@users.noreply.github.com>
2025-09-08 13:29:42 -0500

    j

--- WWW/index.php
+++ WWW/index.php
@@ -3,11 +3,8 @@ $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
     case '/':
-    case '':
-        require __DIR__ . '/home.php';
-        break;
     case '/pages/policy':
-        require __DIR__ . '/pages/policy.php';
+        require __DIR__ . '/pages/policy.html';
         break;
     default:
         http_response_code(404);
